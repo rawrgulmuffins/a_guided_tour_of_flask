0bacc92d43f90bc063e31eeb856a9aae857f8a20
Alex Lord <alord@isilon.com>
2015-06-04 20:14:17 -0700

    Removed heart_beat subtree.

--- heart_beat/heart_beat/development_config.py
+++
@@ -1,9 +0,0 @@
-
-""" -------------------------- DATA BASE CONFINGURATION --------------------"""
-SQLALCHEMY_DATABASE_URI = 'sqlite:////tmp/test.db'
-SQLALCHEMY_ECHO = False
-
-
-""" -------------------------- Flask Application Config --------------------"""
-THREADS_PER_PAGE = 8
-VERSION = "0.0.0.1"
